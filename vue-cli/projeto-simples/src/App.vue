e58a1e47dbf38e07c9aba0adf5ce870b697cf056
Pedro Melo <ppvitorr@gmail.com>
2020-04-23 09:56:18 -0300

    completed vue-cli introduction module

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: 'Teste data usando Vue JS',
+    };
+  },
+}
+</script>
+
+<style>
+  #app {
+    background-color: cadetblue;
+    font-size: 24px;
+  }
+</style>
