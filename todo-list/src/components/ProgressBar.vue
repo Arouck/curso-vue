06dcf9094e7966e3ca4b0aa89b1e659dd0506e9a
Pedro Melo <ppvitorr@gmail.com>
2020-04-26 23:47:10 -0300

    started todo list project

---
+++ todo-list/src/components/ProgressBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="bar">
+        <div id="progress">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
