02eb420a18bca60e81d4f293298a4a6e51754661
Pedro Victor <ppvitorr@gmail.com>
2020-06-23 16:52:12 -0300

    Iniciado módulo de rotas com vue-router

---
+++ rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="usuario-detalhe">
+        <h3>Usuário detalhe</h3>
+        <p><strong>Código: </strong>{{ id }}</p>
+        <router-link tag='button' primario :to='`/usuario/${id}/editar`'>
+            Editar
+        </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    // data() {
+    //     return {
+    //         id: this.$route.params.id,
+    //     }
+    // },
+
+    // watch: {
+    //     $route(to, from) {
+    //         this.id = to.params.id;
+    //     }
+    // },
+}
+</script>
+
+<style>
+
+</style>
---
+++ rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="usuario-editar">
+        <h3>Usuário editar</h3>
+        <hr>
+        <p><strong>Código: </strong>{{ id }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    // data() {
+    //     return {
+    //         id: this.$route.params.id,
+    //     }
+    // },
+
+    // watch: {
+    //     $route(to, from) {
+    //         this.id = to.params.id;
+    //     }
+    // },
+}
+</script>
+
+<style>
+
+</style>
